5cd659fa57d06d5b3d43744f39161fb67c2acc18
Nicholas <niccholaspage@gmail.com>
2016-06-01 23:25:21 -0400

    Move constructor logic to init method

--- src/main/java/com/nicholasnassar/ninja/api/plugin/JavaPlugin.java
+++ src/main/java/com/nicholasnassar/ninja/api/plugin/JavaPlugin.java
@@ -1,14 +1,14 @@
 package com.nicholasnassar.ninja.api.plugin;
 
 public class JavaPlugin implements Plugin {
-    private final PluginDescriptionInfo description;
+    private PluginDescriptionInfo description;
 
-    public JavaPlugin(PluginDescriptionInfo description) {
+    public void init(PluginDescriptionInfo description) {
         this.description = description;
     }
 
     public void onEnable() {
-        //TODO: Implement this
+
     }
 
     public void onDisable() {
